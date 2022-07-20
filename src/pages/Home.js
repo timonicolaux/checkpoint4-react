d12f3706c2c7b43370d60f898fde717894756a1b
Timo Nicolaux <timo.nicolaux@hotmail.fr>
2022-07-20 11:38:37 +0200

    finished base of Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "../styles/Home.css";
+import Vegetables from "../assets/vegetables.jpg";
 
 const Home = () => {
   return (
     <div className="main">
-      <h1 className="title">Home</h1>
-      <h1 className="title">Home</h1>
-      <h1 className="title">Home</h1>
-      <h1 className="title">Home</h1>
-      <h1 className="title">Home</h1>
+      {/* <div className="vegetables-img"></div> */}
+      <div>
+        <img src={Vegetables} alt="vegetables-img" className="vegetables-img" />
+      </div>
+      <div className="main-section">
+        <div className="home-title-div">
+          <h2 className="home-title">
+            Envie de cuisiner, mais en manque d’idées ? Trouvez de l’inspiration
+            sur notre site
+          </h2>
+        </div>
+        <div className="top-recettes-div">
+          <h2 className="top-recettes">NOTRE TOP RECETTES</h2>
+        </div>
+      </div>
     </div>
   );
 };

4d9c0f0cdfc75a411cc8f295ca6bcf2f96a23c65
Timo Nicolaux <timo.nicolaux@hotmail.fr>
2022-07-21 17:09:34 +0200

    added a basic img if no img

--- src/components/RecetteSummary.js
+++ src/components/RecetteSummary.js
@@ -4,7 +4,15 @@ import "../styles/RecetteSummary.css";
 const RecetteSummary = ({ titre, image }) => {
   return (
     <div className="recette-summary">
-      <img src={image} alt={titre} className="recette-img" />
+      <img
+        src={
+          image === ""
+            ? "https://thumbs.dreamstime.com/b/no-image-vector-isolated-white-background-no-image-vector-illustration-isolated-156298619.jpg"
+            : image
+        }
+        alt={titre}
+        className="recette-img"
+      />
       <h1 className="recette-title">{titre}</h1>
     </div>
   );
